b32b49a2e402986ac560171dd79dfc7c0b08ee2d
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-01-12 09:18:17 -0500

    vd

---
+++ Search/src/SearchAlgorithms.java
@@ -0,0 +1,62 @@
+
+public class SearchAlgorithms {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int[] test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[] test2 = {0, 4, 5, 6, 12, 15, 17, 23, 29};
+		
+		System.out.println(binSearch(test1, 3));
+		System.out.println(linSearch(test1, 3));
+		
+		System.out.println(binSearch(test2, 23));
+		System.out.println(linSearch(test2, 23));
+		
+		System.out.println(binSearch(test2, 25));
+		System.out.println(linSearch(test2, 25));
+		
+		System.out.println(binSearch(test1, 10));
+		System.out.println(linSearch(test1, 1));
+		
+		System.out.println(binSearch(test1, 1));
+
+	}
+
+	public static int linSearch(int[] test, int query) 
+	{
+		for(int i = 0; i < test.length; i++)
+		{
+			if(test[i] == query)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int binSearch(int[] test, int query) 
+	{
+		int L = 0;
+		int R = test.length - 1;
+		while(L <= R)
+		{
+			int mid = L + (R - L)/2;
+			if(test[mid] > query)
+			{
+				L = mid;
+			}
+			if (test[mid] < query)
+			{
+				R = mid;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		return -1;
+	}
+	
+}
+
